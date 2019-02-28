1eaa650b891f0c93b7b022b528cea077b58b541b
Jeremy R <lvlgrinddesign@gmail.com>
2019-02-28 07:50:39 -0500

    added notes information

--- note.js
+++ note.js
@@ -9,41 +9,6 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
-const findNotes = function(notes, query) {
-    return notes.filter(function (note, index) {
-        const isTitleMatch = note.title.toLowerCase().includes(query.toLowerCase());
-        const isBodyMatch = note.body.toLowerCase().includes(query.toLowerCase());
-        return isTitleMatch || isBodyMatch;
-    })
-}
-
-
-console.log(findNotes(notes, 'et'));
-
-
-
-
-// const findNote = function (notes, noteTitle) {
-//     return notes.find(function (note, index) {
-//         return note.title.toLowerCase() === noteTitle.toLowerCase()
-//     });
-// }
-
-
-// const findNote = function (notes, noteTitle) {
-//     const index = notes.findIndex(function (note, index) {
-//         return note.title.toLowerCase() === noteTitle.toLowerCase()
-//     });
-//     return notes[index];
-// }
-
-// const note = findNote(notes, 'office modification');
-// console.log(note);
-// console.log(notes.length);
-// console.log(notes);
-
-// const index = notes.findIndex(function (note, index) {
-//     return note.title === 'Habbits to work on';
-// });
-
-// console.log(index);
+const newP = document.createElement('p');
+newP.textContent = 'Hey this is a new paragraph';
+document.querySelector('section').appendChild(newP);
