5540fac745a6a7c62df7ec8bdcec84d58f1032b1
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-01 12:12:38 -0500

    removed the remove all event handler for the remove all button

--- note.js
+++ note.js
@@ -32,12 +32,6 @@ document.querySelector('#create-note').addEventListener('click', function(e) {
     e.target.textContent = 'the button was clicked';
 })
 
-document.querySelector('#remove-all').addEventListener('click', function (e) {
-    document.querySelectorAll('.note').forEach(function (note) {
-        note.remove();
-    })
-})
-
 document.querySelector('#note-search').addEventListener('input', function (e) {
     filters.searchText = e.target.value;
     renderNotes(notes, filters);
