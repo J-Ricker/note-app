783f2e5643c55e9615f6a073e2bf9241509a1eae
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-01 10:00:08 -0500

    cleaned up javaascript code

--- note.js
+++ note.js
@@ -8,18 +8,6 @@ const notes = [{
     title: 'Office modification',
     body: 'Get a new seat'
 }]
-
-const newParagraphs = notes.forEach(function (para) {
-    const p = document.createElement('p');
-    p.textContent = para.title;
-    document.querySelector('section').appendChild(p);
-})
-
-
-const newP = document.createElement('p');
-newP.textContent = 'Hey this is a new paragraph';
-document.querySelector('section').appendChild(newP);
-
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', function(e) {
