db16b6f00b7bc197732afd413d1edfebb07abd85
Jeremy R <lvlgrinddesign@gmail.com>
2019-03-01 09:38:24 -0500

    changed up notes app javascript to display all the objects title field

--- note.js
+++ note.js
@@ -9,6 +9,13 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+const newParagraphs = notes.forEach(function (para) {
+    const p = document.createElement('p');
+    p.textContent = para.title;
+    document.querySelector('section').appendChild(p);
+})
+
+
 const newP = document.createElement('p');
 newP.textContent = 'Hey this is a new paragraph';
 document.querySelector('section').appendChild(newP);
