27c7b08183a6e09ef9932f611d173306660dc48c
J-Ricker <lvlgrinddesign@gmail.com>
2019-03-05 12:47:19 -0500

    Added event listener for title input, body input, and remove button

--- notes-edit.js
+++ notes-edit.js
@@ -1,3 +1,6 @@
+const titleElement = document.querySelector('#note-title').value = note.title;
+const bodyElement = document.querySelector('#note-body').value = note.body;
+const removeElement = document.querySelector('#remove-note');
 const noteId = location.hash.substring(1);
 const notes = getSavedNotes();
 const note = notes.find(function (note) {
@@ -8,6 +11,21 @@ if(note === undefined) {
     location.assign('/index.html');
 }
 
-document.querySelector('#note-title').value = note.title;
-document.querySelector('#note-body').value = note.body;
+titleElement.value = note.title;
+bodyElement.value = note.body;
 
+titleElement.addEventListener('input', function (e) {
+    note.title = e.target.value;
+    savedNotes(notes);
+})
+
+bodyElement.addEventListener('input', function (e) {
+    note.body = e.target.value;
+    savedNotes(notes);
+})
+
+removeElement.addEventListener('click', function (e) {
+    removeNote('note.id');
+    savedNotes(notes);
+    location.assign('/index.html');
+})
